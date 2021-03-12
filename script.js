f1f79ca5f1f6f3938462087101a2a51c2adc880a
Yulia-dev-design <76877408+Yulia-dev-design@users.noreply.github.com>
2021-03-12 13:43:34 +0100

    update
    current position button,
    trying to fix life forcast

--- script.js
+++ script.js
@@ -11,9 +11,35 @@ let days = [
 ];
 let day = days[now.getDay()];
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
 h2.innerHTML = `Now: ${day} ${hours}:${minutes}`;
+
+function displayTemperature(response) {
+  let temperature = document.querySelector("#temperature");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let precipitation;
+  if (response.data.precipitation) {
+    precipitation = response.data.precipitation.value;
+  }
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperature.innerHTML = Math.round(celsiusTemperature);
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
 
 function search(event) {
   event.preventDefault();
@@ -23,6 +49,7 @@ function search(event) {
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -50,29 +77,6 @@ function showTemperatureForLatLong(position) {
 function showTemperatureForCity(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   showTemperatureFromUrl(url);
-}
-
-function showTemperatureFromUrl(url) {
-  let h1 = document.querySelector("h1");
-  console.log(url);
-  axios({
-    method: "get",
-    url: url,
-  }).then(function (response) {
-    h1.innerHTML = `You are in ${response.data.name}`;
-    let temperature = Math.round(response.data.main.temp);
-    let windSpeed = response.data.wind.speed;
-    let iconElement = response.data.weather[0].icon;
-    let description = response.data.weather[0].description;
-    let precipitation;
-    if (response.data.precipitation) {
-      precipitation = response.data.precipitation.value;
-    }
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = `${temperature} â„ƒ`;
-
-    console.log(response);
-  });
 }
 
 function getCurrentPosition() {
