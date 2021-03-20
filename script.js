3ea1e1496a7f9892c9b069cdf1377c004147f53c
Yulia-dev-design <76877408+Yulia-dev-design@users.noreply.github.com>
2021-03-20 20:23:32 +0100

    changes
    HTML: adjusted button current position, submit, date
    Jv:date (does not work)
    temperatureelements,
    CSS: background height, h2 font weight, started to work on Weather forcast

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
-let now = new Date();
-let h2 = document.querySelector("h2");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  dateElement.innerHTML = `last updated: ${day}, ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-h2.innerHTML = `Now: ${day} ${hours}:${minutes}`;
 
 function displayTemperature(response) {
-  let temperature = document.querySelector("#temperature");
-  let city = document.querySelector("#city");
-  let description = document.querySelector("#description");
-  let humidity = document.querySelector("#humidity");
-  let wind = document.querySelector("#wind");
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  //let dateElement = document.querySelector("date");
+  let dateElement = formatDate;
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   let precipitation;
   if (response.data.precipitation) {
     precipitation = response.data.precipitation.value;
   }
+  let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
 
-  temperature.innerHTML = Math.round(celsiusTemperature);
-  city.innerHTML = response.data.name;
-  description.innerHTML = response.data.weather[0].description;
-  humidity.innerHTML = response.data.main.humidity;
-  wind.innerHTML = Math.round(response.data.wind.speed);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(event) {
@@ -47,23 +55,31 @@ function search(event) {
   let cityInput = document.querySelector("#city-input");
   showTemperatureForCity(cityInput.value);
 }
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 32;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 0;
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
+let celsiusTemperature = null;
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
@@ -78,7 +94,20 @@ function showTemperatureForCity(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   showTemperatureFromUrl(url);
 }
+function showTemperatureFromUrl(url) {
+  let h1 = document.querySelector("h1");
+  axios({
+    method: "get",
+    url: url,
+  }).then(function (response) {
+    displayTemperature(response);
+  });
+}
+
+let positionButton = document.querySelector("#positionButton");
+positionButton.addEventListener("click", getCurrentPosition);
 
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showTemperatureForLatLong);
 }
+showTemperatureForCity("Utrecht");
