e75e3077e65d43cffff49deb40fbd4e8f1230c03
Yulia-dev-design <76877408+Yulia-dev-design@users.noreply.github.com>
2021-03-21 17:57:42 +0100

    adjusted
    html: deleted precipitation, added forecast:
    Css: image settings, forecast
    Jv: fixed bugs in date, typos

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -18,26 +22,21 @@ function formatDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  dateElement.innerHTML = `last updated: ${day}, ${hours}:${minutes}`;
+  return `${hours}:${minutes}`;
 }
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  //let dateElement = document.querySelector("date");
-  let dateElement = formatDate;
+  let dateElement = document.querySelector("#date");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let precipitation;
-  if (response.data.precipitation) {
-    precipitation = response.data.precipitation.value;
-  }
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -47,6 +46,26 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+  <div class="col-2">
+  <h3>${formatHours(forecast.dt * 1000)}</h3>
+  <img src="http://openweathermap.org/img/wn/${
+    forecast.weather[0].icon
+  }@2x.png"/>
+  <div class="weather-forecast.main.temp_max)}</strong>${Math.round(
+    forecast.main.temp_min
+  )}
+  </div>
+  </div>
+  `;
+  }
 }
 
 function search(event) {
@@ -86,14 +105,21 @@ celsiusLink.addEventListener("click", convertToCelsius);
 const API_KEY = "83f221ac387e19170ecde933ccac4e98";
 
 function showTemperatureForLatLong(position) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`;
   showTemperatureFromUrl(url);
+
+  url = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`;
+  axios.get(url).then(displayForecast);
 }
 
 function showTemperatureForCity(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   showTemperatureFromUrl(url);
+
+  url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`;
+  axios.get(url).then(displayForecast);
 }
+
 function showTemperatureFromUrl(url) {
   let h1 = document.querySelector("h1");
   axios({
@@ -110,4 +136,3 @@ positionButton.addEventListener("click", getCurrentPosition);
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showTemperatureForLatLong);
 }
-showTemperatureForCity("Utrecht");
